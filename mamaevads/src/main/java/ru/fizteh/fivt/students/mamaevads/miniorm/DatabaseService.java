122042324164addd383a42286242d8627afd7822
Mamaeva Daria <mamaevads@gmail.com>
2015-12-17 20:56:14 +0300

    Merge

--- mamaevads/src/main/java/ru/fizteh/fivt/students/mamaevads/miniorm/DatabaseService.java
+++ mamaevads/src/main/java/ru/fizteh/fivt/students/mamaevads/miniorm/DatabaseService.java
@@ -14,8 +14,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.h2.jdbcx.JdbcConnectionPool;
-
+//import org.h2.jdbcx.JdbcConnectionPool;
 
 public class DatabaseService<T> {
     private Class<T> tableClass;
