a67fde1139da26ae7fdfa43eff869a9eab7b98ef
Mamaeva Daria <mamaevads@gmail.com>
2015-12-19 01:25:43 +0300

    Pls go

---
+++ mamaevads/src/main/java/ru/fizteh/fivt/students/mamaevads/moduletests/library/FakeTwitter.java
@@ -0,0 +1,7 @@
+package ru.fizteh.fivt.students.mamaevads.moduletests.library;
+
+/**
+ * Created by Даша on 02.12.2015.
+ */
+public class FakeTwitter {
+}
--- mamaevads/src/main/java/ru/fizteh/fivt/students/mamaevads/moduletests/library/MakeMassage.java
+++ mamaevads/src/main/java/ru/fizteh/fivt/students/mamaevads/moduletests/library/MakeMassage.java
@@ -37,4 +37,3 @@ public class MakeMassage {
         return message;
     }
 }
-
